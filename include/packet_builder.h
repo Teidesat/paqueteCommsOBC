80c17bb45a96368406164d825c954b17118d119a
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-05-09 23:05:48 +0100

    Basic implementation of packet builder. Only allows adding main header and data field header.

---
+++ include/packet_builder.h
@@ -0,0 +1,56 @@
+/**
+ * @file packet_builder.h
+ * @author Marcos Barrios
+ * @brief Builder for different kinds of packets
+ * @version 0.1
+ * @date 2023-05-09
+ * 
+ * A packet can be either:
+ *    - main header only + app data
+ *    - main header + data field header + app data
+ * 
+ * Then, the app data can be of many different kinds depending on the service.
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef PACKET_BUILDER_H
+#define PACKET_BUILDER_H
+
+#include "packet.h"
+
+class PacketBuilder {
+public:
+  PacketBuilder();
+  ~PacketBuilder();
+
+  /****** getters and setters ******/
+
+  Packet getPacket() const;
+
+  /****** other ******/
+
+  // reset current construction
+  void newPacket();
+
+  /*
+   * It's the mandatory packet header.
+   * dataFielHeader is left implicit.
+   */
+  void addMainHeader(const std::bitset<Packet::VERSION_NUMBER_SIZE>& versionNumber,
+                const std::bitset<Packet::APP_ID_SOURCE_SIZE>& appIdSource,
+                const std::bitset<Packet::APP_ID_DESTINATION_SIZE>& appIdDestination,
+                const Packet::SequenceFlags sequenceControlFlags,
+                const std::bitset<Packet::SEQUENCE_CONTROL_COUNT_SIZE>& sequenceControlCount);
+
+  void addDataFieldHeader(const bool ack,
+              const std::bitset<Packet::SERVICE_TYPE_SIZE>& serviceType,
+              const std::bitset<Packet::SERVICE_SUBTYPE_SIZE>& serviceSubtype);
+
+private:
+  Packet packet_;
+  bool hasAddedMainHeader_;
+};
+
+#endif
---
+++ src/packet_builder.cpp
@@ -0,0 +1,41 @@
+#include "../include/packet_builder.h"
+
+#include <cassert>
+
+PacketBuilder::PacketBuilder() : hasAddedMainHeader_(false) {}
+
+PacketBuilder::~PacketBuilder() {}
+
+Packet PacketBuilder::getPacket() const {
+  assert(hasAddedMainHeader_);
+  return packet_;
+}
+
+// reset current construction
+void PacketBuilder::newPacket() {
+  packet_ = Packet();
+}
+
+void PacketBuilder::addMainHeader(const std::bitset<Packet::VERSION_NUMBER_SIZE>& versionNumber,
+              const std::bitset<Packet::APP_ID_SOURCE_SIZE>& appIdSource,
+              const std::bitset<Packet::APP_ID_DESTINATION_SIZE>& appIdDestination,
+              const Packet::SequenceFlags sequenceControlFlags,
+              const std::bitset<Packet::SEQUENCE_CONTROL_COUNT_SIZE>& sequenceControlCount) {
+  
+  packet_.setVersionNumber(versionNumber);
+  packet_.setAppIdSource(appIdSource);
+  packet_.setAppIdDestination(appIdDestination);
+  packet_.setSequenceControlCount(sequenceControlCount);
+  packet_.setSequenceControlFlags(sequenceControlFlags);
+
+  hasAddedMainHeader_ = true;
+}
+
+void PacketBuilder::addDataFieldHeader(const bool ack,
+              const std::bitset<Packet::SERVICE_TYPE_SIZE>& serviceType,
+              const std::bitset<Packet::SERVICE_SUBTYPE_SIZE>& serviceSubtype) {
+
+  packet_.setACK(ack);
+  packet_.setServiceType(serviceType);
+  packet_.setServiceSubtype(serviceSubtype);
+}
