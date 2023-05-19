22619090e4f256c0401ee9ec7a9881d39455783d
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-05-19 19:37:58 +0100

    Remade readPacket function. Missing app data. Pending writePacket().

--- include/packet_buffer_io.h
+++ include/packet_buffer_io.h
@@ -12,18 +12,6 @@
 
 #include "packet.h"
 
-// I use this for extract field auxiliary function.
-// but since concepts have to be defined at namespace level
-// then I have to define this here.
-template <typename T>
-concept IsOneByte = (sizeof(T) == 1);
-
-// I want the combine function to work with array of 2 bytes or a vector
-// of any amount of bytes.
-template <typename T, typename T2>
-concept IsArrayOrVector = std::is_same_v<T, std::vector<T2>> && IsOneByte<T2> ||
-                          std::is_same_v<T, std::array<T2, 2>> && IsOneByte<T2>;
-
 class PacketBufferIO {
 public:
   PacketBufferIO();
@@ -36,9 +24,12 @@ private:
    *    separate fields.
    * 
    * @param startIndex rightmost = 0
+   * 
+   * @return a word with the field and if there are other bits they are all
+   *    0. For example: for input 0b00100110, startIndex = 5,
+   *    lengthOfField = 3, then output is 0b00000001
    */
-  template<IsOneByte T>
-  std::byte extractFieldFrom(T inputByte, uint8_t startIndex,
+  uint8_t extractFieldFrom(std::byte inputByte, uint8_t startIndex,
       size_t lengthOfField);
 
   /**
@@ -48,16 +39,9 @@ private:
    * For example, when a 16 bits field has 1 bit in a word, 8 in another
    *    and 7 in the next. The result is 3 different bytes, but it should
    *    be just 2.
-   *
-   * @tparam Toutvector the type of the array or vector.
-   * @tparam Tinvector the type of the array or vector.
    * 
-   * @return the combination of the different byte words into fewer.
    */
-  template <typename Tout, typename Toutvector, typename Tin, typename Tinvector>
-    requires IsArrayOrVector<Tout, Toutvector> && IsArrayOrVector<Tin, Tinvector>
-  Tout combineFields(const Tin* inputArray, size_t sizeOfArray);
-  uint16_t combineFields(const uint8_t* inputArray, size_t sizeOfArray);
+  std::array<std::byte, 2> combineFields(const std::byte* inputArray, size_t sizeOfArray);
 };
 
 #endif
--- src/packet_buffer_io.cpp
+++ src/packet_buffer_io.cpp
@@ -12,35 +12,99 @@ PacketBufferIO::PacketBufferIO() {}
  *    fields or not I will increase or not the ptrBuffer.
  * 
  *  Map of byte to fields:
- *  std::byte =  version, type, dfh, 0, source...
- *  std::byte =  ...source, destination, length...
- *  std::byte =  ...length...
- *  std::byte =  ...length, seq. ctrl. flags...
- *  std::byte =  ...seq. ctrl. flags, seq. ctrl. count...
- *  std::byte =  ...seq. ctrl. count...
- *  std::byte =  ...seq. ctrl. count, ccsds, pus, ack...
- *  std::byte =  ...ack, type...
- *  std::byte =  ...type, subtype...
- *  std::byte =  ...subtype app data(...)
+ * XXX X X XXX...	  |  std::byte =  version, type, dfh, 0, source...
+ * ...XX XXXXX X...	|  std::byte =  ...source, destination, length...
+ * ...XXXXXXXX...	  |  std::byte =  ...length...
+ * ...XXXXXXX X...	|  std::byte =  ...length, seq_ctrl_flags...
+ * ...X XXXXXXX...	|  std::byte =  ...seq_ctrl_flags, seq_ctrl_count...
+ * ...XXXXXXXX...	  |  std::byte =  ...seq_ctrl_count...
+ * ...X X XXX XXX...|  std::byte =  ...seq_ctrl_count, ccsds, pus, ack...
+ * ...X XXXXXXX...	|  std::byte =  ...ack, type...
+ * ...X XXXXXXX...	|  std::byte =  ...type, subtype...
+ * ...X XXXXXXX...	|  std::byte =  ...subtype app data(...)
  * 
  * Note: extractFieldFrom() assumes that the rightmost bit is index 0 on
  *    each byte.
  */
 Packet PacketBufferIO::readPacket(const std::byte* ptrBuffer, std::size_t size) {
-  const uint8_t versionNumber =
-    static_cast<uint8_t>((extractFieldFrom(ptrBuffer[0], 5, 3)));
+
+  // **** TODO: measure if coarcing to bitset<> is faster for bit indexing than current method.
+
+  const uint8_t versionNumber = (extractFieldFrom(ptrBuffer[0], 5, 3));
+
   Packet::Bool8Enum dataFieldHeader;
-  uint8_t appIdSource;
-  uint8_t appIdDestination;
-  Packet::SequenceFlags sequenceControlFlags;
+
+  if (extractFieldFrom(ptrBuffer[0], 4, 1) == 1) {
+    dataFieldHeader = Packet::Bool8Enum::TRUE;
+  } else {
+    dataFieldHeader = Packet::Bool8Enum::FALSE;
+  }
+
+  // extract field gives: 00000XXX, 000000XX, then do:
+  // 000XXX00, 000000XX -> do OR.
+  uint8_t appIdSourcePartial1 = extractFieldFrom(ptrBuffer[0], 0, 3);
+  uint8_t appIdSourcePartial2 = static_cast<uint8_t>(ptrBuffer[1] >> 6);
+  uint8_t appIdSource = (appIdSourcePartial1 << 2) | appIdSourcePartial2;
+
+  uint8_t appIdDestination = extractFieldFrom(ptrBuffer[1], 1, 5);
+
+  // for length, because it's spread in 3 different bytes, I extract 3 not 2
+  uint8_t lengthPartial1 = static_cast<uint8_t>(ptrBuffer[1] << 7);
+  uint8_t lengthPartial2 = static_cast<uint8_t>(ptrBuffer[2] >> 1);
+  uint8_t lengthPartial3 = static_cast<uint8_t>(ptrBuffer[3] >> 1);
+
+  // because length is 2 bytes and not 1 then calculate first least significant
+  // and after that the more significant, then memcpy on the allocation.
+  // ptrBuffer[2] is all length content, I need to push the Y to ptrBuffer[3]:
+  // XXXXXXXY, XXXXXXX0 -> Y0000000, XXXXXXX0 -> Y0000000, 0XXXXXXX -> do OR.
+  uint8_t lengthLeastSignificant =
+      static_cast<uint8_t>(ptrBuffer[2] << 7) | (lengthPartial3 >> 1);
+
+  // 0000000X, XXXXXXXX -> X0000000, XXXXXXXX -> X0000000, 0XXXXXXX -> do OR.
+  uint8_t lengthMostSignificant = (lengthPartial1 << 8) | (lengthPartial2 >> 1);
+
+  // **** TODO: measure whether it is faster to cast or to use a pointer with memcpy.
+  uint16_t length =
+      (static_cast<uint16_t>(lengthMostSignificant) << 8) | lengthLeastSignificant;
+
+  uint8_t flags1 = extractFieldFrom(ptrBuffer[3], 0, 1);
+  uint8_t flags2 = extractFieldFrom(ptrBuffer[4], 7, 1);
+  uint8_t flags = (flags1 << 1) | flags2;
+  Packet::SequenceFlags sequenceControlFlags =
+      static_cast<Packet::SequenceFlags>(flags);
+  
   uint16_t sequenceControlCount;
-  uint16_t length;
+
+  // 0XXXXXXX, XXXXXXXX -> XXXXXXX0, XXXXXXXX -> XXXXXXX0, 0000000X -> do OR.
+  uint8_t countMostSignificant = static_cast<uint8_t>((ptrBuffer[4] << 1)) |
+                                 static_cast<uint8_t>((ptrBuffer[5] >> 7));
+  
+  // XXXXXXXX, X0000000 -> XXXXXXX0, X0000000 -> do OR.
+  uint8_t countLeastSignificant = static_cast<uint8_t>((ptrBuffer[5] << 1)) |
+                                  static_cast<uint8_t>((ptrBuffer[6] >> 7));
 
   Packet::Bool8Enum ccsds;
-  uint8_t pusVersion;
+  if (extractFieldFrom(ptrBuffer[6], 6, 1) == 1) {
+    ccsds = Packet::Bool8Enum::TRUE;
+  } else {
+    ccsds = Packet::Bool8Enum::FALSE;
+  }
+  uint8_t pusVersion = extractFieldFrom(ptrBuffer[6], 3, 3);
+
+  uint8_t ackPartial1 = extractFieldFrom(ptrBuffer[6], 0, 3);
+  uint8_t ackPartial2 = static_cast<uint8_t>(ptrBuffer[7] >> 7);
+  uint8_t ackNumber = (ackPartial1 << 1) | ackPartial2;
   Packet::Bool8Enum ack;
-  uint8_t serviceType;
-  uint8_t serviceSubtype;
+  if (ackNumber == 1) {
+    ack = Packet::Bool8Enum::TRUE;
+  } else {
+    ack = Packet::Bool8Enum::FALSE;
+  }
+
+  uint8_t serviceType = static_cast<uint8_t>(ptrBuffer[7] << 1) |
+                        static_cast<uint8_t>(ptrBuffer[8] >> 7);
+  uint8_t serviceSubtype = static_cast<uint8_t>(ptrBuffer[8] << 1) |
+                           static_cast<uint8_t>(ptrBuffer[9] >> 7);
 
   std::array<std::byte, Packet::APP_DATA_SIZE> appData;
 
@@ -135,7 +199,6 @@ void PacketBufferIO::writePacket(std::byte* ptrBuffer, Packet& packet) {
 
   const auto& packetErrorControl = packet.getPacketErrorControl();
 
-
   std::memcpy(ptrBuffer, &versionNumber, sizeof(versionNumber));
   ptrBuffer += sizeof(versionNumber);
 
@@ -179,20 +242,19 @@ void PacketBufferIO::writePacket(std::byte* ptrBuffer, Packet& packet) {
   ptrBuffer += sizeof(packetErrorControl);
 }
 
-template<IsOneByte T>
-std::byte PacketBufferIO::extractFieldFrom(T inputByte, uint8_t startIndex,
+uint8_t PacketBufferIO::extractFieldFrom(std::byte inputByte, uint8_t startIndex,
     size_t lengthOfField) {
-  return (inputByte >> (startIndex % 8)) & (std::pow(2, lengthOfField) - 1);
+  return static_cast<uint8_t>(
+    (inputByte >> (startIndex % 8)) & std::byte((std::pow(2, lengthOfField) - 1)));
 }
 
-template <typename Tout, typename Toutvector, typename Tin, typename Tinvector>
-  requires IsArrayOrVector<Tout, Toutvector> && IsArrayOrVector<Tin, Tinvector>
-Tout combineFields(const Tin* inputArray, size_t sizeOfArray) {
-  return Tout;
-}
+// std::array<std::byte, 2> PacketBufferIO::combineFields(const std::byte* inputArray,
+//     size_t sizeOfArray) {
+  
+// }
 
-uint16_t combineFields(const uint8_t* inputArray, size_t sizeOfArray) {
+// uint16_t combineFields(const uint8_t* inputArray, size_t sizeOfArray) {
 
-  return 0;
-}
+//   return 0;
+// }
 
