9413d85a4670d12bbeecf497c76f80c7130c0426
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-05-20 21:10:39 +0100

    Changed unit test of packet buffer io to use TEST_F to have a common setup for different tests.

--- test/packet_buffer_io_test.cpp
+++ test/packet_buffer_io_test.cpp
@@ -6,55 +6,65 @@
 #include "../include/packet.h"
 #include "../include/packet_buffer_io.h"
 
-TEST(PacketBufferIO, PacketBufferReadWorksWithDataFieldHeader) {
-  /**
-   * Define a packet of verification service acceptance success
-   *    (subtype 1) that is as follows:
-   * 
-   *  version type dfh   appid length sequence_control ccsds pus_version ack type subtype  error
-   *    0      0    1     1, 2    9      (0b11, 2)        0       1        0   13     1     794 
-   * 
-   * length = 9 - 1 = (ccsds, pus, ack, type, subtype)(3 octet) +
-   *  (app. id)(2 octets according to ECSS-70-E-41A) +
-   *  (sequence control)(2 octets) + (error control)(2 octets) + (-1)
-   * 
-   * Sequence control contains flags and count. flags = 0b11 means
-   *    stand_alone. Then count = 1.
-   * 
-   * error control = 0 because I won't use a CRC8 algorithm during unit
-   *  testing.
-   */
-  std::byte packetBuffer[Packet::PACKET_SIZE];
-  for (size_t i = 0; i < Packet::PACKET_SIZE; ++i) {
-    packetBuffer[i] = std::byte{0};
+// I want to have a common setup for a few tests so I do test fixture
+class PacketBufferIOWithDataFieldHeaderTest : public testing::Test {
+protected:
+  void SetUp() override {
+    /**
+     * Define a packet of verification service acceptance success
+     *    (subtype 1) that is as follows:
+     * 
+     *  version type dfh   appid length sequence_control ccsds pus_version ack type subtype  error
+     *    0      0    1     1, 2    9      (0b11, 2)        0       1        0   13     1     794 
+     * 
+     * length = 9 - 1 = (ccsds, pus, ack, type, subtype)(3 octet) +
+     *  (app. id)(2 octets according to ECSS-70-E-41A) +
+     *  (sequence control)(2 octets) + (error control)(2 octets) + (-1)
+     * 
+     * Sequence control contains flags and count. flags = 0b11 means
+     *    stand_alone. Then count = 1.
+     * 
+     * error control = 0 because I won't use a CRC8 algorithm during unit
+     *  testing.
+     */
+    for (size_t i = 0; i < Packet::PACKET_SIZE; ++i) {
+      packetBuffer[i] = std::byte{0};
+    }
+    packetBuffer[0] = std::byte{0b00001000};	// XXX X X 0 XX... | std::byte = version, type, dfh, 0, source...
+    packetBuffer[1] = std::byte{0b00100010};	// ...XXX XXXXX    | std::byte = ...source, destination
+    packetBuffer[2] = std::byte{0b00000000};	// XXXXXXXX...     | std::byte = length...
+    packetBuffer[3] = std::byte{0b00001001};	// ...XXXXXXXX     | std::byte = ...length
+    packetBuffer[4] = std::byte{0b11000000};	// XX XXXXXX...    | std::byte = seq_ctrl_flags, seq_ctrl_count...
+    packetBuffer[5] = std::byte{0b00000010};	// ...XXXXXXXX     | std::byte = ...seq_ctrl_count
+    packetBuffer[6] = std::byte{0b00010000};	// X XXX XXXX      | std::byte = ccsds, pus, ack
+    packetBuffer[7] = std::byte{0b00001101};	// XXXXXXXX        | std::byte = type
+    packetBuffer[8] = std::byte{0b00000001};	// XXXXXXXX        | std::byte = subtype
+
+    // app. id copy of the telecommand this packet is confirming as
+    // acceptance success since this test is not precedent to a telecommand,
+    // I choose arbitrarily that the telecommand was from ground (app id 2) to an
+    // imaginary cubesat module of app id 1.
+    packetBuffer[9] = std::byte{0b00000000};	//
+    packetBuffer[10] = std::byte{0b01000001};	// copy of app. id. of telecommand
+
+    // sequence control copy of the telecommand this packet is confirming.
+    // I set it arbitrarily it's standalone with a value of 1 for the telecommand
+    // being the first ever sent by this service, with this verification
+    // being the second ever.
+    packetBuffer[11] = std::byte{0b11000000}; //
+    packetBuffer[12] = std::byte{0b00000010}; // copy of count of telecommand
+    // sequence control
+    packetBuffer[13] = std::byte{0b00011010};
+    packetBuffer[14] = std::byte{0b00000011};
   }
-  packetBuffer[0] = std::byte{0b00001000};	// XXX X X 0 XX... | std::byte = version, type, dfh, 0, source...
-  packetBuffer[1] = std::byte{0b00100010};	// ...XXX XXXXX    | std::byte = ...source, destination
-  packetBuffer[2] = std::byte{0b00000000};	// XXXXXXXX...     | std::byte = length...
-  packetBuffer[3] = std::byte{0b00001001};	// ...XXXXXXXX     | std::byte = ...length
-  packetBuffer[4] = std::byte{0b11000000};	// XX XXXXXX...    | std::byte = seq_ctrl_flags, seq_ctrl_count...
-  packetBuffer[5] = std::byte{0b00000010};	// ...XXXXXXXX     | std::byte = ...seq_ctrl_count
-  packetBuffer[6] = std::byte{0b00010000};	// X XXX XXXX      | std::byte = ccsds, pus, ack
-  packetBuffer[7] = std::byte{0b00001101};	// XXXXXXXX        | std::byte = type
-  packetBuffer[8] = std::byte{0b00000001};	// .XXXXXXX...     | std::byte = subtype
 
-  // app. id copy of the telecommand this packet is confirming as
-  // acceptance success since this test is not precedent to a telecommand,
-  // I will suppose the telecommand was from ground (app id 2) to an
-  // imaginary cubesat module of app id 1.
-  packetBuffer[9] = std::byte{0b00000000};	//
-  packetBuffer[10] = std::byte{0b01000001};	// copy of app. id. of telecommand
+  // ****** members of the class ******
+  std::byte packetBuffer[Packet::PACKET_SIZE];
+};
 
-  // sequence control copy of the telecommand this packet is confirming.
-  // I suppose it's standalone with a value of 1 for the telecommand
-  // being the first ever sent by this service, with this verification
-  // being the second ever.
-  packetBuffer[11] = std::byte{0b11000000}; //
-  packetBuffer[12] = std::byte{0b00000010}; // copy of count of telecommand
-  // sequence control
-  packetBuffer[13] = std::byte{0b00011010};
-  packetBuffer[14] = std::byte{0b00000011};
-
+// Because first argument is the fixture class, the protected members are
+// available on the test (packetBuffer):
+TEST_F(PacketBufferIOWithDataFieldHeaderTest, ReadsProperly) {
   PacketBufferIO io;
   Packet packet = io.readPacket(packetBuffer, Packet::PACKET_SIZE);
   EXPECT_EQ(packet.getVersionNumber(), 0);
@@ -75,52 +85,76 @@ TEST(PacketBufferIO, PacketBufferReadWorksWithDataFieldHeader) {
   EXPECT_EQ(packet.getPacketErrorControl()[1], std::byte{0b00011010});
 }
 
-TEST(PacketBufferIO, PacketBufferReadWorksWithoutDataFieldHeader) {
-  /**
-   * Define a packet of verification service acceptance success
-   *    (subtype 1) that is as follows:
-   * 
-   *  version type dfh   appid length sequence_control error
-   *    0      0    0     1, 2    9      (0b11, 2)      794 
-   * 
-   * length = 9 - 1 = (ccsds, pus, ack, type, subtype)(3 octet) +
-   *  (app. id)(2 octets according to ECSS-70-E-41A) +
-   *  (sequence control)(2 octets) + (error control)(2 octets) + (-1)
-   * 
-   * Sequence control contains flags and count. flags = 0b11 means
-   *    stand_alone. Then count = 1.
-   * 
-   * error control = 0 because I won't use a CRC8 algorithm during unit
-   *  testing.
-   */
-  std::byte packetBuffer[Packet::PACKET_SIZE];
+TEST_F(PacketBufferIOWithDataFieldHeaderTest, WritesCorrectly) {
+  PacketBufferIO io;
+  Packet packet = io.readPacket(packetBuffer, Packet::PACKET_SIZE);
+
+  std::byte packetBuffer2[Packet::PACKET_SIZE];
   for (size_t i = 0; i < Packet::PACKET_SIZE; ++i) {
-    packetBuffer[i] = std::byte{0};
+    packetBuffer2[i] = std::byte{0};
   }
-  packetBuffer[0] = std::byte{0b00001000};	// XXX X X 0 XX... | std::byte = version, type, dfh, 0, source...
-  packetBuffer[1] = std::byte{0b00100010};	// ...XXX XXXXX    | std::byte = ...source, destination
-  packetBuffer[2] = std::byte{0b00000000};	// XXXXXXXX...     | std::byte = length...
-  packetBuffer[3] = std::byte{0b00001001};	// ...XXXXXXXX     | std::byte = ...length
-  packetBuffer[4] = std::byte{0b11000000};	// XX XXXXXX...    | std::byte = seq_ctrl_flags, seq_ctrl_count...
-  packetBuffer[5] = std::byte{0b00000010};	// ...XXXXXXXX     | std::byte = ...seq_ctrl_count
+  io.writePacket(packetBuffer2, packet);
+  
+  for (size_t i = 0; i < Packet::PACKET_SIZE; ++i) {
+    EXPECT_EQ(packetBuffer2[i], packetBuffer[i]);
+  };
+}
 
-  // app. id copy of the telecommand this packet is confirming as
-  // acceptance success since this test is not precedent to a telecommand,
-  // I will suppose the telecommand was from ground (app id 2) to an
-  // imaginary cubesat module of app id 1.
-  packetBuffer[9] = std::byte{0b00000000};	//
-  packetBuffer[10] = std::byte{0b01000001};	// copy of app. id. of telecommand
+// *** Now testing without data field header ***
 
-  // sequence control copy of the telecommand this packet is confirming.
-  // I suppose it's standalone with a value of 1 for the telecommand
-  // being the first ever sent by this service, with this verification
-  // being the second ever.
-  packetBuffer[11] = std::byte{0b11000000}; //
-  packetBuffer[12] = std::byte{0b00000010}; // copy of count of telecommand
-  // sequence control
-  packetBuffer[13] = std::byte{0b00011010};
-  packetBuffer[14] = std::byte{0b00000011};
+class PacketBufferIOWithoutDataFieldHeaderTest : public testing::Test {
+protected:
+  void SetUp() override {
+    /**
+     * Define a packet of verification service acceptance success
+     *    (subtype 1) that is as follows:
+     * 
+     *  version type dfh   appid length sequence_control error
+     *    0      0    0     1, 2    9      (0b11, 2)      794 
+     * 
+     * length = 9 - 1 = (ccsds, pus, ack, type, subtype)(3 octet) +
+     *  (app. id)(2 octets according to ECSS-70-E-41A) +
+     *  (sequence control)(2 octets) + (error control)(2 octets) + (-1)
+     * 
+     * Sequence control contains flags and count. flags = 0b11 means
+     *    stand_alone. Then count = 1.
+     * 
+     * error control = 0 because I won't use a CRC8 algorithm during unit
+     *  testing.
+     */
+    for (size_t i = 0; i < Packet::PACKET_SIZE; ++i) {
+      packetBuffer[i] = std::byte{0};
+    }
+    packetBuffer[0] = std::byte{0b00001000};	// XXX X X 0 XX... | std::byte = version, type, dfh, 0, source...
+    packetBuffer[1] = std::byte{0b00100010};	// ...XXX XXXXX    | std::byte = ...source, destination
+    packetBuffer[2] = std::byte{0b00000000};	// XXXXXXXX...     | std::byte = length...
+    packetBuffer[3] = std::byte{0b00001001};	// ...XXXXXXXX     | std::byte = ...length
+    packetBuffer[4] = std::byte{0b11000000};	// XX XXXXXX...    | std::byte = seq_ctrl_flags, seq_ctrl_count...
+    packetBuffer[5] = std::byte{0b00000010};	// ...XXXXXXXX     | std::byte = ...seq_ctrl_count
 
+    // app. id copy of the telecommand this packet is confirming as
+    // acceptance success since this test is not precedent to a telecommand,
+    // I will choose arbitrarily the telecommand was from ground (app id 2) to an
+    // imaginary cubesat module of app id 1.
+    packetBuffer[9] = std::byte{0b00000000};	//
+    packetBuffer[10] = std::byte{0b01000001};	// copy of app. id. of telecommand
+
+    // sequence control copy of the telecommand this packet is confirming.
+    // I choose arbitrarily it's standalone with a value of 1 for the telecommand
+    // being the first ever sent by this service, with this verification
+    // being the second ever.
+    packetBuffer[11] = std::byte{0b11000000}; //
+    packetBuffer[12] = std::byte{0b00000010}; // copy of count of telecommand
+    // sequence control
+    packetBuffer[13] = std::byte{0b00011010};
+    packetBuffer[14] = std::byte{0b00000011};
+  }
+
+  // ****** members of the class ******
+  std::byte packetBuffer[Packet::PACKET_SIZE];
+};
+
+TEST_F(PacketBufferIOWithoutDataFieldHeaderTest, ReadsCorrectly) {
   PacketBufferIO io;
   Packet packet = io.readPacket(packetBuffer, Packet::PACKET_SIZE);
   EXPECT_EQ(packet.getVersionNumber(), 0);
@@ -142,117 +176,9 @@ TEST(PacketBufferIO, PacketBufferReadWorksWithoutDataFieldHeader) {
   EXPECT_EQ(packet.getPacketErrorControl()[1], std::byte{0b00011010});
 }
 
-TEST(PacketBufferIO, PacketBufferWriteWorks) {
-  /**
-   * Define a packet of verification service acceptance success
-   *    (subtype 1) that is as follows:
-   * 
-   *  version type dfh   appid length sequence_control ccsds pus_version ack type subtype  error
-   *    0      0    1     1, 2    9      (0b11, 2)        0       1        0   13     1      0
-   * 
-   * length = 9 - 1 = (ccsds, pus, ack, type, subtype)(3 octet) +
-   *  (app. id)(2 octets according to ECSS-70-E-41A) +
-   *  (sequence control)(2 octets) + (error control)(2 octets) + (-1)
-   * 
-   * Sequence control contains flags and count. flags = 0b11 means
-   *    stand_alone. Then count = 1.
-   * 
-   * error control = 0 because I won't use a CRC8 algorithm during unit
-   *  testing.
-   */
-  std::byte packetBuffer[Packet::PACKET_SIZE];
-  for (size_t i = 0; i < Packet::PACKET_SIZE; ++i) {
-    packetBuffer[i] = std::byte{0};
-  }
-  packetBuffer[0] = std::byte{0b00001000};	// XXX X X 0 XX... | std::byte = version, type, dfh, 0, source...
-  packetBuffer[1] = std::byte{0b00100010};	// ...XXX XXXXX    | std::byte = ...source, destination
-  packetBuffer[2] = std::byte{0b00000000};	// XXXXXXXX...     | std::byte = length...
-  packetBuffer[3] = std::byte{0b00001001};	// ...XXXXXXXX     | std::byte = ...length
-  packetBuffer[4] = std::byte{0b11000000};	// XX XXXXXX...    | std::byte = seq_ctrl_flags, seq_ctrl_count...
-  packetBuffer[5] = std::byte{0b00000010};	// ...XXXXXXXX     | std::byte = ...seq_ctrl_count
-  packetBuffer[6] = std::byte{0b00010000};	// X XXX XXXX      | std::byte = ccsds, pus, ack
-  packetBuffer[7] = std::byte{0b00001101};	// XXXXXXXX        | std::byte = type
-  packetBuffer[8] = std::byte{0b00000001};	// .XXXXXXX...     | std::byte = subtype
-
-  // app. id copy of the telecommand this packet is confirming as
-  // acceptance success since this test is not precedent to a telecommand,
-  // I will suppose the telecommand was from ground (app id 2) to an
-  // imaginary cubesat module of app id 1.
-  packetBuffer[9] = std::byte{0b00000000};	//
-  packetBuffer[10] = std::byte{0b01000001};	// copy of app. id. of telecommand
-
-  // sequence control copy of the telecommand this packet is confirming.
-  // I suppose it's standalone with a value of 1 for the telecommand
-  // being the first ever sent by this service, with this verification
-  // being the second ever.
-  packetBuffer[11] = std::byte{0b11000000}; //
-  packetBuffer[12] = std::byte{0b00000010}; // copy of count of telecommand
-
-  // sequence control
-  packetBuffer[13] = std::byte{0b00011010};
-  packetBuffer[14] = std::byte{0b00000011};
+TEST_F(PacketBufferIOWithoutDataFieldHeaderTest, WritesCorrectly) {
   PacketBufferIO io;
   Packet packet = io.readPacket(packetBuffer, Packet::PACKET_SIZE);
-
-  std::byte packetBuffer2[Packet::PACKET_SIZE];
-  for (size_t i = 0; i < Packet::PACKET_SIZE; ++i) {
-    packetBuffer2[i] = std::byte{0};
-  }
-  io.writePacket(packetBuffer2, packet);
-  
-  for (size_t i = 0; i < Packet::PACKET_SIZE; ++i) {
-    EXPECT_EQ(packetBuffer2[i], packetBuffer[i]);
-  };
-}
-
-TEST(PacketBufferIO, PacketBufferWriteWorksWithoutDataFieldHeader) {
-  /**
-   * Define a packet of verification service acceptance success
-   *    (subtype 1) that is as follows:
-   * 
-   *  version type dfh   appid length sequence_control error
-   *    0      0    0     1, 2    9      (0b11, 2)      794 
-   * 
-   * length = 9 - 1 = (ccsds, pus, ack, type, subtype)(3 octet) +
-   *  (app. id)(2 octets according to ECSS-70-E-41A) +
-   *  (sequence control)(2 octets) + (error control)(2 octets) + (-1)
-   * 
-   * Sequence control contains flags and count. flags = 0b11 means
-   *    stand_alone. Then count = 1.
-   * 
-   * error control = 0 because I won't use a CRC8 algorithm during unit
-   *  testing.
-   */
-  std::byte packetBuffer[Packet::PACKET_SIZE];
-  for (size_t i = 0; i < Packet::PACKET_SIZE; ++i) {
-    packetBuffer[i] = std::byte{0};
-  }
-  packetBuffer[0] = std::byte{0b00001000};	// XXX X X 0 XX... | std::byte = version, type, dfh, 0, source...
-  packetBuffer[1] = std::byte{0b00100010};	// ...XXX XXXXX    | std::byte = ...source, destination
-  packetBuffer[2] = std::byte{0b00000000};	// XXXXXXXX...     | std::byte = length...
-  packetBuffer[3] = std::byte{0b00001001};	// ...XXXXXXXX     | std::byte = ...length
-  packetBuffer[4] = std::byte{0b11000000};	// XX XXXXXX...    | std::byte = seq_ctrl_flags, seq_ctrl_count...
-  packetBuffer[5] = std::byte{0b00000010};	// ...XXXXXXXX     | std::byte = ...seq_ctrl_count
-
-  // app. id copy of the telecommand this packet is confirming as
-  // acceptance success since this test is not precedent to a telecommand,
-  // I will suppose the telecommand was from ground (app id 2) to an
-  // imaginary cubesat module of app id 1.
-  packetBuffer[9] = std::byte{0b00000000};	//
-  packetBuffer[10] = std::byte{0b01000001};	// copy of app. id. of telecommand
-
-  // sequence control copy of the telecommand this packet is confirming.
-  // I suppose it's standalone with a value of 1 for the telecommand
-  // being the first ever sent by this service, with this verification
-  // being the second ever.
-  packetBuffer[11] = std::byte{0b11000000}; //
-  packetBuffer[12] = std::byte{0b00000010}; // copy of count of telecommand
-  // sequence control
-  packetBuffer[13] = std::byte{0b00011010};
-  packetBuffer[14] = std::byte{0b00000011};
-  PacketBufferIO io;
-  Packet packet = io.readPacket(packetBuffer, Packet::PACKET_SIZE);
-
 
   std::byte packetBuffer2[Packet::PACKET_SIZE];
   for (size_t i = 0; i < Packet::PACKET_SIZE; ++i) {
