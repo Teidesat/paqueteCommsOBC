6e246a0373e9ddc577ad6f7252bb2fa71060dd02
alu0101056944@ull.edu.es <alu0101056944@ull.edu.es>
2023-05-20 21:44:45 +0100

    Added parametrized test for different ccsds, ack value combinations on writePacket() with datafieldheader test.

--- test/packet_buffer_io_test.cpp
+++ test/packet_buffer_io_test.cpp
@@ -62,6 +62,9 @@ protected:
   std::byte packetBuffer[Packet::PACKET_SIZE];
 };
 
+// This is the test that will be created for each ack, ccsds combination
+// by using INSTANTIATE_TEST_SUITE_P()
+//
 // Because first argument is the fixture class, the protected members are
 // available on the test (packetBuffer):
 TEST_F(PacketBufferIOWithDataFieldHeaderTest, ReadsProperly) {
@@ -74,7 +77,6 @@ TEST_F(PacketBufferIOWithDataFieldHeaderTest, ReadsProperly) {
   EXPECT_EQ(packet.getLength(), 9);
   EXPECT_EQ(packet.getSequenceControlFlags(), Packet::SequenceFlags::STAND_ALONE);
   EXPECT_EQ(packet.getSequenceControlCount(), 2);
-
   EXPECT_EQ(packet.getCCSDS(), Packet::Bool8Enum::FALSE);
   EXPECT_EQ(packet.getPUSVersion(), 1);
   EXPECT_EQ(packet.getAck(), Packet::Bool8Enum::FALSE);
@@ -85,7 +87,135 @@ TEST_F(PacketBufferIOWithDataFieldHeaderTest, ReadsProperly) {
   EXPECT_EQ(packet.getPacketErrorControl()[1], std::byte{0b00011010});
 }
 
-TEST_F(PacketBufferIOWithDataFieldHeaderTest, WritesCorrectly) {
+ // other CCSDS, ACK variations:
+
+TEST_F(PacketBufferIOWithDataFieldHeaderTest, ReadsProperlyCCSDSTRUEACKFALSE) {
+  packetBuffer[6] = std::byte{0b10010000};
+  PacketBufferIO io;
+  Packet packet = io.readPacket(packetBuffer, Packet::PACKET_SIZE);
+  EXPECT_EQ(packet.getVersionNumber(), 0);
+  EXPECT_EQ(packet.getDataFieldHeader(), Packet::Bool8Enum::TRUE);
+  EXPECT_EQ(packet.getAppIdSource(), 1);
+  EXPECT_EQ(packet.getAppIdDestination(), 2);
+  EXPECT_EQ(packet.getLength(), 9);
+  EXPECT_EQ(packet.getSequenceControlFlags(), Packet::SequenceFlags::STAND_ALONE);
+  EXPECT_EQ(packet.getSequenceControlCount(), 2);
+  EXPECT_EQ(packet.getCCSDS(), Packet::Bool8Enum::TRUE); // changed
+  EXPECT_EQ(packet.getPUSVersion(), 1);
+  EXPECT_EQ(packet.getAck(), Packet::Bool8Enum::FALSE);
+  EXPECT_EQ(packet.getServiceType(), 13);
+  EXPECT_EQ(packet.getServiceSubtype(), 1);
+
+  EXPECT_EQ(packet.getPacketErrorControl()[0], std::byte{0b00000011});
+  EXPECT_EQ(packet.getPacketErrorControl()[1], std::byte{0b00011010});
+}
+
+TEST_F(PacketBufferIOWithDataFieldHeaderTest, ReadsProperlyCCSDSTRUEACKTRUE) {
+  packetBuffer[6] = std::byte{0b10010001};
+  PacketBufferIO io;
+  Packet packet = io.readPacket(packetBuffer, Packet::PACKET_SIZE);
+  EXPECT_EQ(packet.getVersionNumber(), 0);
+  EXPECT_EQ(packet.getDataFieldHeader(), Packet::Bool8Enum::TRUE);
+  EXPECT_EQ(packet.getAppIdSource(), 1);
+  EXPECT_EQ(packet.getAppIdDestination(), 2);
+  EXPECT_EQ(packet.getLength(), 9);
+  EXPECT_EQ(packet.getSequenceControlFlags(), Packet::SequenceFlags::STAND_ALONE);
+  EXPECT_EQ(packet.getSequenceControlCount(), 2);
+  EXPECT_EQ(packet.getCCSDS(), Packet::Bool8Enum::TRUE); // changed
+  EXPECT_EQ(packet.getPUSVersion(), 1);
+  EXPECT_EQ(packet.getAck(), Packet::Bool8Enum::TRUE); // changed
+  EXPECT_EQ(packet.getServiceType(), 13);
+  EXPECT_EQ(packet.getServiceSubtype(), 1);
+
+  EXPECT_EQ(packet.getPacketErrorControl()[0], std::byte{0b00000011});
+  EXPECT_EQ(packet.getPacketErrorControl()[1], std::byte{0b00011010});
+}
+
+TEST_F(PacketBufferIOWithDataFieldHeaderTest, ReadsProperlyCCSDSFALSEACKTRUE) {
+  packetBuffer[6] = std::byte{0b00010001};
+  PacketBufferIO io;
+  Packet packet = io.readPacket(packetBuffer, Packet::PACKET_SIZE);
+  EXPECT_EQ(packet.getVersionNumber(), 0);
+  EXPECT_EQ(packet.getDataFieldHeader(), Packet::Bool8Enum::TRUE);
+  EXPECT_EQ(packet.getAppIdSource(), 1);
+  EXPECT_EQ(packet.getAppIdDestination(), 2);
+  EXPECT_EQ(packet.getLength(), 9);
+  EXPECT_EQ(packet.getSequenceControlFlags(), Packet::SequenceFlags::STAND_ALONE);
+  EXPECT_EQ(packet.getSequenceControlCount(), 2);
+  EXPECT_EQ(packet.getCCSDS(), Packet::Bool8Enum::FALSE);
+  EXPECT_EQ(packet.getPUSVersion(), 1);
+  EXPECT_EQ(packet.getAck(), Packet::Bool8Enum::TRUE); // changed
+  EXPECT_EQ(packet.getServiceType(), 13);
+  EXPECT_EQ(packet.getServiceSubtype(), 1);
+
+  EXPECT_EQ(packet.getPacketErrorControl()[0], std::byte{0b00000011});
+  EXPECT_EQ(packet.getPacketErrorControl()[1], std::byte{0b00011010});
+}
+
+// I want to test the write correctly with different ack and ccsds parameters
+// too, so I write a parametrized version of the test:
+
+// Because I want to test for ack = true|false and ccsds = true|false,
+// I can then use a TestWithParam. I just need to invoke GetParam() to get the
+// std::byte that will be at packetBuffer[6], which is the word that contains
+// both ccsds and ack. Then I use INSTANTIATE_TEST_SUITE_P to create the
+// different tests for the different ack, ccsds combinations.
+class PacketBufferIOWithDataFieldHeaderParametrizedTest : public testing::TestWithParam<std::byte> {
+protected:
+  void SetUp() override {
+    /**
+     * Define a packet of verification service acceptance success
+     *    (subtype 1) that is as follows:
+     * 
+     *  version type dfh   appid length sequence_control ccsds pus_version ack type subtype  error
+     *    0      0    1     1, 2    9      (0b11, 2)        0       1        0   13     1     794 
+     * 
+     * length = 9 - 1 = (ccsds, pus, ack, type, subtype)(3 octet) +
+     *  (app. id)(2 octets according to ECSS-70-E-41A) +
+     *  (sequence control)(2 octets) + (error control)(2 octets) + (-1)
+     * 
+     * Sequence control contains flags and count. flags = 0b11 means
+     *    stand_alone. Then count = 1.
+     * 
+     * error control = 0 because I won't use a CRC8 algorithm during unit
+     *  testing.
+     */
+    for (size_t i = 0; i < Packet::PACKET_SIZE; ++i) {
+      packetBuffer[i] = std::byte{0};
+    }
+    packetBuffer[0] = std::byte{0b00001000};	// XXX X X 0 XX... | std::byte = version, type, dfh, 0, source...
+    packetBuffer[1] = std::byte{0b00100010};	// ...XXX XXXXX    | std::byte = ...source, destination
+    packetBuffer[2] = std::byte{0b00000000};	// XXXXXXXX...     | std::byte = length...
+    packetBuffer[3] = std::byte{0b00001001};	// ...XXXXXXXX     | std::byte = ...length
+    packetBuffer[4] = std::byte{0b11000000};	// XX XXXXXX...    | std::byte = seq_ctrl_flags, seq_ctrl_count...
+    packetBuffer[5] = std::byte{0b00000010};	// ...XXXXXXXX     | std::byte = ...seq_ctrl_count
+    packetBuffer[6] = GetParam();	            // X XXX XXXX      | std::byte = ccsds, pus, ack
+    packetBuffer[7] = std::byte{0b00001101};	// XXXXXXXX        | std::byte = type
+    packetBuffer[8] = std::byte{0b00000001};	// XXXXXXXX        | std::byte = subtype
+
+    // app. id copy of the telecommand this packet is confirming as
+    // acceptance success since this test is not precedent to a telecommand,
+    // I choose arbitrarily that the telecommand was from ground (app id 2) to an
+    // imaginary cubesat module of app id 1.
+    packetBuffer[9] = std::byte{0b00000000};	//
+    packetBuffer[10] = std::byte{0b01000001};	// copy of app. id. of telecommand
+
+    // sequence control copy of the telecommand this packet is confirming.
+    // I set it arbitrarily it's standalone with a value of 1 for the telecommand
+    // being the first ever sent by this service, with this verification
+    // being the second ever.
+    packetBuffer[11] = std::byte{0b11000000}; //
+    packetBuffer[12] = std::byte{0b00000010}; // copy of count of telecommand
+    // sequence control
+    packetBuffer[13] = std::byte{0b00011010};
+    packetBuffer[14] = std::byte{0b00000011};
+  }
+
+  // ****** members of the class ******
+  std::byte packetBuffer[Packet::PACKET_SIZE];
+};
+
+TEST_P(PacketBufferIOWithDataFieldHeaderParametrizedTest, WritesCorrectly) {
   PacketBufferIO io;
   Packet packet = io.readPacket(packetBuffer, Packet::PACKET_SIZE);
 
@@ -99,6 +229,15 @@ TEST_F(PacketBufferIOWithDataFieldHeaderTest, WritesCorrectly) {
     EXPECT_EQ(packetBuffer2[i], packetBuffer[i]);
   };
 }
+
+// WriteSuite is an arbitrary name, doesn't matter, the suit is not important.
+INSTANTIATE_TEST_SUITE_P(WriteSuite, PacketBufferIOWithDataFieldHeaderParametrizedTest,
+    testing::Values(
+      std::byte{0b00010000}, // ccsds = FALSE, ack = FALSE
+      std::byte{0b00010001}, // ccsds = FALSE, ack = TRUE
+      std::byte{0b10010000}, // ccsds = TRUE, ack = FALSE
+      std::byte{0b00010001}  // ccsds = TRUE, ack = TRUE
+    ));
 
 // *** Now testing without data field header ***
 
